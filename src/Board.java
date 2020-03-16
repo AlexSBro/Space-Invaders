a4cd33d76517914da7f8cb4dbe9519795fcd42f4
alexandersamuelbrown <alicks.b@gmail.co>
2020-03-16 13:33:31 -0700

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,160 @@
+
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.Timer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.imageio.*;
+import java.awt.image.*;
+import java.io.*;
+
+
+public class Board  extends JPanel implements Runnable, MouseListener
+{
+    boolean ingame = true;
+    private Dimension d;
+    int BOARD_WIDTH=500;
+    int BOARD_HEIGHT=500;
+    int x = 0;
+    BufferedImage img;
+    String message = "Click Board to Start";
+    private Thread animator;
+
+
+    public Board()
+    {
+        addKeyListener(new TAdapter());
+        addMouseListener(this);
+        setFocusable(true);
+        d = new Dimension(BOARD_WIDTH, BOARD_HEIGHT);
+        setBackground(Color.black);
+
+           /*
+             try {
+                img = ImageIO.read(this.getClass().getResource("mount.jpg"));
+            } catch (IOException e) {
+                 System.out.println("Image could not be read");
+            // System.exit(1);
+            }
+            */
+        if (animator == null || !ingame) {
+            animator = new Thread(this);
+            animator.start();
+        }
+
+
+        setDoubleBuffered(true);
+    }
+
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+
+        g.setColor(Color.white);
+        g.fillRect(0, 0, d.width, d.height);
+//g.fillOval(x,y,r,r);
+
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics metr = this.getFontMetrics(small);
+        g.setColor(Color.black);
+        g.setFont(small);
+        g.drawString(message, 10, d.height-60);
+
+        if (ingame) {
+
+
+
+
+
+            // g.drawImage(img,0,0,200,200 ,null);
+
+
+
+        }
+        Toolkit.getDefaultToolkit().sync();
+        g.dispose();
+    }
+    private class TAdapter extends KeyAdapter {
+
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+
+        }
+
+        public void keyPressed(KeyEvent e) {
+//System.out.println( e.getKeyCode());
+            // message = "Key Pressed: " + e.getKeyCode();
+            int key = e.getKeyCode();
+            if(key==39){
+
+            }
+
+
+
+        }
+
+    }
+
+
+
+
+    public void mousePressed(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    public void run() {
+
+        long beforeTime, timeDiff, sleep;
+
+        beforeTime = System.currentTimeMillis();
+        int animationDelay = 500;
+        long time =
+                System.currentTimeMillis();
+        while (true) {//infinite loop
+            // spriteManager.update();
+            repaint();
+            try {
+                time += animationDelay;
+                Thread.sleep(Math.max(0,time -
+                        System.currentTimeMillis()));
+            }catch (InterruptedException e) {
+                System.out.println(e);
+            }//end catch
+        }//end while loop
+
+
+
+
+    }//end of run
+
+}//end of class
---
+++ src/Starter.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Starter extends JFrame {
+
+    public Starter() {
+        add(new Board());
+        setTitle("Board");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        new Starter();
+    }
+
+}
