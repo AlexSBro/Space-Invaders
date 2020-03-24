caecb4b64948613782cdd410bb5e884ea625afe6
alexandersamuelbrown <alicks.b@gmail.co>
2020-03-24 16:00:56 -0700

    Working on explosion.

--- src/Explosion.java
+++ src/Explosion.java
@@ -1,29 +1,54 @@
+import java.awt.*;
+
 public class Explosion extends GameObject {
 
     public Explosion(int x, int y, int width, int height, int speed, GameObjectManager gameObjectManager) {
         super(x, y, width, height, speed, 5, gameObjectManager);
 
-        this.spriteDrawing = new char[][] {
 
-                {'X','X','X','X','X','X','X','X','X','X'},
-                {' ',' ',' ',' ','X','X',' ',' ',' ',' '},
-                {'X','X','X','X','X','X','X','X','X','X'},
-                {' ',' ',' ',' ','X','X',' ',' ',' ',' '},
-                {'X','X','X','X','X','X','X','X','X','X'},
-                {' ',' ',' ',' ','X','X',' ',' ',' ',' '},
-                {'X','X','X','X','X','X','X','X','X','X'},
-                {' ','X','X','X','X','X','X','X','X',' '},
-                {'X','X','X','X','X','X','X','X','X','X'},
-                {' ',' ','X','X','X','X','X','X',' ',' '},
-        };
     }
 
     @Override
     public void tick() {
         super.tick();
 
+        switch (health){
+            case 5:
+                this.spriteDrawing = new char[][] {
+
+                        {'X','X','X','X','X','X','X','X','X','X'},
+                        {' ',' ',' ',' ','X','X',' ',' ',' ',' '},
+                        {'X','X','X','X','X','X','X','X','X','X'},
+                        {' ',' ',' ',' ','X','X',' ',' ',' ',' '},
+                        {'X','X','X','X','X','X','X','X','X','X'},
+                        {' ',' ',' ',' ','X','X',' ',' ',' ',' '},
+                        {'X','X','X','X','X','X','X','X','X','X'},
+                        {' ','X','X','X','X','X','X','X','X',' '},
+                        {'X','X','X','X','X','X','X','X','X','X'},
+                        {' ',' ','X','X','X','X','X','X',' ',' '},
+                };
+                break;
+            case 4:
+                break;
+            case 3:
+                break;
+            case 2:
+                break;
+            case 1:
+                break;
+
+        }
+
+
         health -= 1;
+
+        if (health == 0)
+            removeSelf();
 
     }
 
+    @Override
+    protected Color getColor() {
+        return ColorBuilder.buildRed(getHealthAlpha());
+    }
 }
